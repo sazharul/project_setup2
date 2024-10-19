cfeceeb3ff9c7c4a0ecc1d8aa31f62136f7b2f42
MD. Azharul Islam <113032991+sazharul@users.noreply.github.com>
2024-10-20 05:04:48 +0600

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    root: path.resolve(__dirname, 'src'),
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
+    server: {
+        port: 8080,
+        hot: true
+    }
+});
