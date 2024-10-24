58409403d348158ab286e1203d5ba7e62f266331
MD. Azharul Islam <113032991+sazharul@users.noreply.github.com>
2024-10-25 03:12:33 +0600

    mobile menu add

--- src/js/custome.js
+++ src/js/custome.js
@@ -2,6 +2,14 @@
 //     x.classList.toggle("change");
 // }
 
-window.toggleMenu = function(x) {
+window.toggleMenu = function (x) {
     x.classList.toggle("change");
+    // Toggle the hamburger animation
+
+    // Toggle the mobile menu visibility
+    const menuSection = document.querySelector('.mobile_menu_section');
+    menuSection.classList.toggle('menu-open');
+
+    const body = document.querySelector('body');
+    body.classList.toggle('no-scroll');
 }
